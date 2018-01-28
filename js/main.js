ed7ab78c1f539222102f43bdaba805a81bdb2e95
Alan Boglioli <alan.boglioli@gmail.com>
2018-01-27 21:28:57 -0300

    importa slides.min.js con require() en main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 import $ from 'jquery';
-import * as slide from './slides.min.js';
 
-// (() => {  
-// })();
+window.$ = $;
+window.jQuery = $;
+
+require('./slides.min');
 
 (function(){
-
-    
-    $('.slider').slidesjs({
-      
-      play: {
-      active: true,
-      effect: "slider",
-      interval: 6000,
-      auto: true,
-      swap: true,
-      pauseOnHover: false,
-      restartDelay: 2500
-      }
-    });
-  
-
-})()
+  $('.slider').slidesjs({
+    play: {
+    active: true,
+    effect: "slider",
+    interval: 6000,
+    auto: true,
+    swap: true,
+    pauseOnHover: false,
+    restartDelay: 2500
+    }
+  });
+})();
