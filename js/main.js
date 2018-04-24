04d697f5b56e3767736cacf20a01a698efbf42f9
juancho <juancho14.jk@gmail.com>
2018-04-24 19:08:41 -0300

    agregado animacion al abrir empresa y emprededor

--- js/main.js
+++ js/main.js
@@ -100,13 +100,23 @@ require('./slides.min');
       },function(){
         $("#empresa").html("TENGO <br class='m-hide l-hide'> UNA EMPRESA");
       });
+      $("#empresa").hover(function(){
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-down'> </span> ");
+      },function(){
+        $("#empresa").html("¡CREZCAMOS!");
+      });
       
     } else{
       document.getElementById("divEmpresa").classList.add("box-active");
       $("#divEmpresa").hover(function(){
-        $("#empresa").html("¡CREZCAMOS!");
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-up'> </span>");
       },function(){
-        $("#empresa").html("¡CREZCAMOS!");
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-up'> </span>");
+      });
+      $("#empresa").hover(function(){
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-up'> </span> ");
+      },function(){
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-up'> </span>");
       });
       
      
@@ -118,17 +128,27 @@ require('./slides.min');
       },function(){
         $("#emprendedor").html("SOY <br class='m-hide l-hide'> EMPRENDEDOR");
       });
+      $("#emprendedor").hover(function(){
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-down'> </span> ");
+      },function(){
+        $("#emprendedor").html("¡EMPRENDAMOS!");
+      });
 
     
 
     }else{
       document.getElementById("divEmprendedor").classList.add("box-active");
       $("#divEmprendedor").hover(function(){
-        $("#emprendedor").html("¡EMPRENDAMOS!");
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-up'> </span> ");
       },function(){
-        $("#emprendedor").html("¡EMPRENDAMOS!");
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-up'> </span> ");
       });
-
+      $("#emprendedor").hover(function(){
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-up'> </span> ");
+      },function(){
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-up'> </span>");
+      });
+      
      
     };
   }
@@ -141,9 +161,14 @@ require('./slides.min');
       emprendedorActive = false;
       empresaControl();
       $('#clienteDescriptionEmpresa').toggle("slow");
-      $("#empresa").html("¡CREZCAMOS!");
       $("#emprendedor").html("SOY <br class='m-hide l-hide'> EMPRENDEDOR");
+      
+      if(empresaActive == true){
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-up'> </span>");
 
+      } else {
+        $("#empresa").html("¡CREZCAMOS! <span class='arrow-down'> </span>");
+      }
       if($('#clienteDescriptionEmprendedor').show()){
         $('#clienteDescriptionEmprendedor').hide();
       }
@@ -154,9 +179,16 @@ require('./slides.min');
       empresaActive = false;
       empresaControl();
       $('#clienteDescriptionEmprendedor').toggle("slow");
-      $("#emprendedor").html("¡EMPRENDAMOS!");
+      
       $("#empresa").html("TENGO UNA <br class='m-hide l-hide'> EMPRESA");
 
+      if(emprendedorActive == true){
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-up'> </span>");
+
+      } else {
+        $("#emprendedor").html("¡EMPRENDAMOS! <span class='arrow-down'> </span>");
+      }
+      
       if($('#clienteDescriptionEmpresa').show()){
         $('#clienteDescriptionEmpresa').hide();
       }
